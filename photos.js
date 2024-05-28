2a621b1e5cff99e68f258e1b73afc450114fdc44
addsoupbase <162063292+addsoupbase@users.noreply.github.com>
2024-05-28 07:33:36 -0400

    Update photos.js

--- photos.js
+++ photos.js
@@ -43,7 +43,8 @@ let aya = new Avatar('aya', 'Aya'),
     khaotic = new Avatar("khaotic","khaotic good"),
     kae = new Avatar("kae"),
     rohan = new Avatar("rohan"),
-    bbg = new Avatar("bbg","bbg.avarice")
+    bbg = new Avatar("bbg","bbg.avarice"),
+    rainmint = new Avatar ("rainmint")
 
 let gorebyss = new Sprite('gorebyssspritesheet', 9),
 phione = new Sprite('phionesprite', 10),
