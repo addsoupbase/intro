aedf16b46d5ce8aff95bec992a55472f36b7c831
addsoupbase <162063292+addsoupbase@users.noreply.github.com>
2024-06-28 02:46:16 -0400

    Update photos.js

--- photos.js
+++ photos.js
@@ -68,7 +68,8 @@ let aya = new Avatar('aya', 'Aya'),
     lexi = new Avatar("lexi"),
     babs = new Avatar("babs"),
     elipoopsrainbows= new Avatar("elipoopsrainbows"),
-    birdie=new Avatar("birdie")
+    birdie=new Avatar("birdie"),
+    elen=new Avatar("elenfnf1")
 
 let gorebyss = new Sprite('gorebyssspritesheet', 9),
     phione = new Sprite('phionesprite', 10),
